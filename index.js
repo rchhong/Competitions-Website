fa5b13063c15476ba5029c92f931d3f828fe5a79
Ryan Chhong <ryanchhong@gmail.com>
2018-02-26 17:49:05 -0600

    Fixed css

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var soln = {
 };
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(express.static(__dirname + '/static/'));
 app.get('/',
   (req, res) => {
     res.sendFile(path.join(__dirname + '/static/1.html'));
