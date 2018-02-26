c99b175181a5c0c5cabbd18d776738019454dae0
Ryan Chhong <ryanchhong@gmail.com>
2018-02-26 16:37:59 -0600

    Fixed Form Submission

--- index.js
+++ index.js
@@ -6,26 +6,34 @@ const bodyParser = require('body-parser');
 var soln = {
           1 : 'ayy',
           2 : 'lmao'
-        };
-app.use(bodyParser.json());
+};
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/',
   (req, res) => {
-    res.sendFile(path.join(__dirname + '/static/1.html'))
-  });
+    res.sendFile(path.join(__dirname + '/static/1.html'));
+});
+
+app.get('/helloworld',
+  (req, res) => {
+    console.log("hello world");
+});
 
 app.post('/:problem', (req, res) =>
-  {
-    var problemNum = req.params.problem
-    var userInput = req.body.answer
+{
+    var problemNum = req.params.problem;
+    var userInput = req.body.answer;
+    console.log(problemNum);
+    console.log(req.body.answer);
     //console.log(problemNum)
     if(soln[problemNum] == userInput) {
-      res.sendFile(path.join(__dirname + '/static/' + (parseInt(problemNum)+1) + '.html'))
-      res.status(202)
+      res.sendFile(path.join(__dirname + '/static/' + (parseInt(problemNum)+1) + '.html'));
+      res.status(202);
     }
     else {
       res.redirect('back');
-      res.status(420)
+      res.status(420);
     }
   });
 
