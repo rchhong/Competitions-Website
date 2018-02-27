145736b3465f3589e0541b0ac8108f619f6294b5
Ryan Chhong <ryanchhong@gmail.com>
2018-02-26 21:48:13 -0600

    implement actual solutions

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 var soln = {
-          1 : 'ayy',
-          2 : 'lmao',
-          3 : 'lmao'
+          1 : '3 8 2 5 s 1 9 s',
+          2 : '8 8 8 2 2 5 5 6 8 8 8 2 2 5 5 6 -1 9999999 1 1 2 2 999 998 997 996 995 994 993 992 991 990 5 9 8 7 6 5 4 3 2 1 0 2 2 2 1 1 1 8 8 8 4',
+          3 : '2 80 n 0 15 1 16 2 17 3 18 4 19 n 1 41 3 43 40 80 50 90 63 103'
 };
 
 app.use(bodyParser.urlencoded({ extended: true }));
